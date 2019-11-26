ca4187bc524790976e1f68310c12ed827aa4de11
leo <leo.dalloz@etu.univ-amu.fr>
2019-11-26 16:40:33 +0100

    suppression d'echecsLeo

--- echecsLeo/Piece.cpp
+++
@@ -1,9 +0,0 @@
-#include "Piece.h"
-
-//getters
-char Piece::getCarac()      {return myCarac;}
-pairCoord Piece::getCoord() {return myCoord;}
-Color Piece::getColor()     {return myColor;}
-
-//setters
-void Piece::setCoord(const pairCoord &coord) {myCoord = coord;}
