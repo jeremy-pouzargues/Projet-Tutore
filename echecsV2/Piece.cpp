b03f8a890ce240ce016e18068b680594e7dd6d61
laurent-vouriot <laurent.vouriot@etu.univ-amu.fr>
2019-11-26 16:19:08 +0100

    grand nettoyage

--- echecsV2/Piece.cpp
+++
@@ -1,9 +0,0 @@
-#include "Piece.h"
-
-//getters
-std::string Piece::getName()  const {return myName;}
-Color       Piece::getColor() const {return myColor;}
-pairCoord   Piece::getCoord()          const {return myCoord;}
-
-//setters
-void        Piece::setCoord(const pairCoord & coord) {myCoord = coord;}
