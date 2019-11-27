2a6b4c8872ee620c663a0ac899c39c209cce7456
p18003049 <jeremy.pouzargues@etu.univ-amu.fr>
2019-11-27 15:15:40 +0100

    implémentation de la classe Knight avec legalMoves

--- echecs/Knight.cpp
+++ echecs/Knight.cpp
@@ -1,5 +1,4 @@
 #include "headers/Knight.h"
-
 #include "headers/CExc.h"
 
 using namespace std;
@@ -15,5 +14,41 @@ Knight::Knight(const Color & color,const pairCoord & coord)
 
 vector<pairCoord> Knight::legalMoves(const vector<vector<shared_ptr<Piece>>> & matrix)
 {
-    //à faire
-}
+    vector<pairCoord> VMoves;
+    unsigned line = this->getCoord().first;
+    unsigned column = this->getCoord().second;
+
+    Color thisColor;
+
+    this->getColor() == white ? thisColor = white : thisColor = black;
+
+    if (line != 0)
+    {
+        if (line != 1)
+        {
+            if (column != 0 && matrix[line - 2][column - 1]->getColor() != thisColor)
+                VMoves.push_back(matrix[line - 2][column - 1]->getCoord());
+            if (column != 7 && matrix[line - 2][column + 1]->getColor() != thisColor)
+                VMoves.push_back(matrix[line - 2][column + 1]->getCoord());
+        }
+        if (column != 0 && column != 1 && matrix[line - 1][column - 2]->getColor() != thisColor)
+            VMoves.push_back(matrix[line - 1][column - 2]->getCoord());
+        if (column != 7 && column != 6 && matrix[line - 1][column + 2]->getColor() != thisColor)
+            VMoves.push_back(matrix[line - 1][column + 2]->getCoord());
+    }
+    if (line != 7)
+    {
+        if (line != 6)
+        {
+            if (column != 0 && matrix[line + 2][column - 1]->getColor() != thisColor)
+                VMoves.push_back(matrix[line + 2][column - 1]->getCoord());
+            if (column != 7 && matrix[line + 2][column + 1]->getColor() != thisColor)
+                VMoves.push_back(matrix[line + 2][column + 1]->getCoord());
+        }
+        if (column != 0 && column != 1 && matrix[line + 1][column - 2]->getColor() != thisColor)
+            VMoves.push_back(matrix[line + 1][column - 2]->getCoord());
+        if (column != 7 && column != 6 && matrix[line + 1][column + 2]->getColor() != thisColor)
+            VMoves.push_back(matrix[line + 1][column + 2]->getCoord());
+    }
+    return VMoves;
+}// legalMoves()
