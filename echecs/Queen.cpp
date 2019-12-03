adfc08dfc53b73e2440136a0d686e44f7a4b0a1c
leo <leo.dalloz@etu.univ-amu.fr>
2019-12-03 14:48:41 +0100

    Modif debug

--- echecs/Queen.cpp
+++ echecs/Queen.cpp
@@ -39,8 +39,8 @@ vector <pairCoord> Queen::legalMoves(const vector<vector<shared_ptr<Piece>>> & m
         }
     }
 
-//    //Test diagonale haut droite
-//    //Impossible si la reine est situé tout en haut ou à droite de l'échiquiuer
+    //    //Test diagonale haut droite
+    //    //Impossible si la reine est situé tout en haut ou à droite de l'échiquiuer
     if(this->myCoord.first != 0 && this->myCoord.second != 7)
     {
         line = this->myCoord.first - 1;
@@ -56,8 +56,8 @@ vector <pairCoord> Queen::legalMoves(const vector<vector<shared_ptr<Piece>>> & m
         }
     }
 
-//    //Test diagonale bas droite
-//    //Impossible si la reine est situé tout en bas ou à droite de l'échiquiuer
+    //    //Test diagonale bas droite
+    //    //Impossible si la reine est situé tout en bas ou à droite de l'échiquiuer
     if(this->myCoord.first != 7 && this->myCoord.second != 7)
     {
         line = this->myCoord.first + 1;
@@ -73,8 +73,8 @@ vector <pairCoord> Queen::legalMoves(const vector<vector<shared_ptr<Piece>>> & m
         }
     }
 
-//    //Test diagonale bas gauche
-//    //Impossible si la reine est situé tout en bas ou à gauche de l'échiquiuer
+    //    //Test diagonale bas gauche
+    //    //Impossible si la reine est situé tout en bas ou à gauche de l'échiquiuer
     if(this->myCoord.first != 7 && this->myCoord.second != 0)
     {
         line = this->myCoord.first + 1;
@@ -92,66 +92,57 @@ vector <pairCoord> Queen::legalMoves(const vector<vector<shared_ptr<Piece>>> & m
 
     column  = this->getCoord().second;
 
-//      //Si la reine est en haut elle ne pourra pas aller en haut
-      if(this->getCoord().first != 0) {
-          line = this->getCoord().first - 1;
+          //Si la reine est en haut elle ne pourra pas aller en haut
+    if(this->getCoord().first != 0) {
+        line = this->getCoord().first - 1;
 
-          while(matrix[line][column]->getColor() != this->getColor()) {
-              VMoves.push_back(pairCoord(line,column));
-              if(line == 0)break;
-              if (matrix[line][column]->getColor() == colorOpponent) break;
-              --line;
-          }//      if(this->getCoord().second != 7) {
-          //          column  = this->getCoord().second + 1;
-          //          //vers la gauche
-          //          while(matrix[line][column]->getColor() != this->getColor()) {
-          //              VMoves.push_back(pairCoord(line,column));
-          //              if( column == 7 ) break;
-          //              if (matrix[line][column]->getColor() == colorOpponent) break;
-          //              ++column;
-          //          }
-          //      }
-      }
+        while(matrix[line][column]->getColor() != this->getColor()) {
+            VMoves.push_back(pairCoord(line,column));
+            if(line == 0)break;
+            if (matrix[line][column]->getColor() == colorOpponent) break;
+            --line;
+        }
+    }
 
-//      //Si la reine est en bas elle ne pourra pas aller en haut
-      if(this->getCoord().first != 7) {
-          line = this->getCoord().first + 1;
+          //Si la reine est en bas elle ne pourra pas aller en bas
+    if(this->getCoord().first != 7) {
+        line = this->getCoord().first + 1;
 
-          //test de cases disponibles dans une colonne vers le bas
-          while(matrix[line][column]->getColor() != this->getColor()) {
-              VMoves.push_back(pairCoord(line,column));
-              if( line == 7 )break;
-              if (matrix[line][column]->getColor() == colorOpponent) break;
-              ++line;
-          }
-      }
+        //test de cases disponibles dans une colonne vers le bas
+        while(matrix[line][column]->getColor() != this->getColor()) {
+            VMoves.push_back(pairCoord(line,column));
+            if( line == 7 )break;
+            if (matrix[line][column]->getColor() == colorOpponent) break;
+            ++line;
+        }
+    }
 
-       line = this->getCoord().first;
+    line = this->getCoord().first;
 
-//      // tests horizontaux
-//      //Si la reine est à gauche elle ne pourra pas aller à gauche
-      if(this->getCoord().second != 0) {
-          column  = this->getCoord().second - 1;
-          // vers la droite
-            while(matrix[line][column]->getColor() != this->getColor())
-            {
-              VMoves.push_back(pairCoord(line,column));
-              if(column == 0 ) break;
-              if (matrix[line][column]->getColor() == colorOpponent) break;
-              --column;
-          }
-      }
+          // tests horizontaux
+          //Si la reine est à gauche elle ne pourra pas aller à gauche
+    if(this->getCoord().second != 0) {
+        column  = this->getCoord().second - 1;
+        // vers la droite
+        while(matrix[line][column]->getColor() != this->getColor())
+        {
+            VMoves.push_back(pairCoord(line,column));
+            if(column == 0 ) break;
+            if (matrix[line][column]->getColor() == colorOpponent) break;
+            --column;
+        }
+    }
 
-//      // collé à gauche
-      if(this->getCoord().second != 7) {
-          column  = this->getCoord().second + 1;
-          //vers la gauche
-          while(matrix[line][column]->getColor() != this->getColor()) {
-              VMoves.push_back(pairCoord(line,column));
-              if( column == 7 ) break;
-              if (matrix[line][column]->getColor() == colorOpponent) break;
-              ++column;
-          }
-      }
-      return VMoves;
+    //      // collé à gauche
+    if(this->getCoord().second != 7) {
+        column  = this->getCoord().second + 1;
+        //vers la gauche
+        while(matrix[line][column]->getColor() != this->getColor()) {
+            VMoves.push_back(pairCoord(line,column));
+            if( column == 7 ) break;
+            if (matrix[line][column]->getColor() == colorOpponent) break;
+            ++column;
+        }
+    }
+    return VMoves;
 }
