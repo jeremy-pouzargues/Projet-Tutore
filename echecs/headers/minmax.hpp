cdce95f3e93f64efa2c6388e171af4dc60906c5c
laurent-vouriot <laurent.vouriot@etu.univ-amu.fr>
2019-12-12 16:06:20 +0100

    debug

--- echecs/headers/minmax.hpp
+++ echecs/headers/minmax.hpp
@@ -8,7 +8,7 @@
 
 int evaluation(const ChessBoard chessboard)
 {
-    //std::cout << "dans evaluation" << std::endl;
+    std::cout << "dans evaluation" << std::endl;
     return 1;
 }
 
@@ -37,17 +37,26 @@ int minmax(ChessBoard & chessboard,const int & depth,const bool & maximizingPlay
                 // pour chaque coup possible par pièce
                 for (pairCoord possibleMove : piece->legalMoves(chessboard.getChessboard()))
                 {
-                    // on garde les coordonées actuelles de la pîece
-                    pairCoord actualCoord = piece->getCoord();
+
+                    std::cout << piece->getName() << " " << piece->getCoord().first  << " " << piece->getCoord().second << " "
+                              << piece->getColor() <<" "  << possibleMove.first << " " << possibleMove.second <<  std::endl;
+
+
+
+                    // on garde dans un tampon l'échiquier actuel
+                    Matrix    actualChessboard  = chessboard.getChessboard();
+                    Matrix    actualDeadPiece   = chessboard.getMyDeadPiece();
                     // on la deplace
-                    chessboard.move(possibleMove,actualCoord);
+                    chessboard.move(possibleMove,piece->getCoord());
                     // on réévalue l'echiquier après ce déplacement
                     int eval = minmax(chessboard,depth-1,!maximizingPlayer);
                     // on regarde si le score est plus élevé que le score max actuelle
                     // si oui ce coup est plus intéréssant
                     maxEval  = std::max(maxEval,eval);
-                    // on remet la pièce à sa position d'origine
-                    chessboard.move(actualCoord,piece->getCoord());
+                    // on remet l'echiquier à sa position d'origine
+                    chessboard.setChessboard(actualChessboard);
+                    chessboard.setDeadPiece(actualDeadPiece);
+
                 }
            }
         }
@@ -67,11 +76,14 @@ int minmax(ChessBoard & chessboard,const int & depth,const bool & maximizingPlay
              {
                for (pairCoord possibleMove : piece->legalMoves(chessboard.getChessboard()))
                {
-                   pairCoord actualCoord = piece->getCoord();
-                   chessboard.move(possibleMove,actualCoord);
+                   Matrix    actualChessboard  = chessboard.getChessboard();
+                   Matrix    actualDeadPiece   = chessboard.getMyDeadPiece();
+                   chessboard.move(possibleMove,piece->getCoord());
                    int eval = minmax(chessboard,depth-1,maximizingPlayer);
                    minEval  = std::min(minEval,eval);
-                   chessboard.move(actualCoord,piece->getCoord());
+
+                   chessboard.setChessboard(actualChessboard);
+                   chessboard.setDeadPiece(actualDeadPiece);
                }
              }
            }
