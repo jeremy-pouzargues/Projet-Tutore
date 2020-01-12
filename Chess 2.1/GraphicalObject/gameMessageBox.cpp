bb027ae917db8f85d215792b25ca2df427876ab8
François <Francois.al-haddad-siderikoudis@etu.univ-amu.fr>
2020-01-12 21:19:39 +0100

    Suppression ancienne version

--- Chess 2.1/GraphicalObject/gameMessageBox.cpp
+++
@@ -1,9 +0,0 @@
-#include "gameMessageBox.h"
-
-gameMessageBox::gameMessageBox(QString innerText) : signal(false)
-{
-    this->setWindowFlag(Qt::FramelessWindowHint);
-    this->setText(innerText);
-    this->setStandardButtons(QMessageBox::Yes|QMessageBox::No);
-    this->exec();
-}
